ea49446e9901f17f317a1311d01b51493708b887
Tyler Spencer <taspencer7@gmail.com>
2017-08-21 17:50:26 -0700

    Update delete_all_UG.py

--- delete_all_UG.py
+++ delete_all_UG.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python
 
-## Purpose: 
-##  	delete all users and/or groups
-## Authors:
-##  	tyler.spencer@thoughtspot.com
-## Created:
-##  	14-May-2017
+"""
+Copyright 2017 ThoughtSpot
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS 
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT 
+OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 
 import sys, requests, json, csv, argparse, socket, time
 from collections import defaultdict
@@ -14,9 +24,9 @@ from collections import defaultdict
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-thoughtspot_host = 'http://10.85.77.112'
-username = 'tsadmin'
-password = 'admin'
+thoughtspot_host = ''
+username = ''
+password = ''
 
 session = requests.session()
 
