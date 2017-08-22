f4c67f30c75f08bde4a35f6e08de7d90d1eb50d8
Tyler Spencer <taspencer7@gmail.com>
2017-08-21 17:52:58 -0700

    Update request_data.py

--- request_data.py
+++ request_data.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python
 
+"""
+Copyright 2017 ThoughtSpot
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS 
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT 
+OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
 
 ## Purpose: 
 ##  	request data from pinboard in ThoughtSpot
@@ -13,10 +28,10 @@ import sys, requests, json, socket, csv, argparse
 from collections import defaultdict
 
 #thoughtspot_host = 'http://' + socket.gethostname()
-thoughtspot_host = 'http://172.31.24.136'
-username = 'portal'
-password = 'portal'
-pinboardId = "6017d81a-e33f-48ab-b943-1538a5b8526c"
+thoughtspot_host = ''
+username = ''
+password = ''
+pinboardId = ''
 
 
 #session cookies
