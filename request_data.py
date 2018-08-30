f796d97c4d09817102c2ae9a0db042fcf83addf9
Tyler Spencer <taspencer7@gmail.com>
2018-08-30 08:26:59 -0700

    Update request_data.py

--- request_data.py
+++ request_data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 """
-Copyright 2017 ThoughtSpot
+Copyright 2018 ThoughtSpot
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
 files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
